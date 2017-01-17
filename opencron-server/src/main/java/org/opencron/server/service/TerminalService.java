4292030f30281caeaa155aa9ffdd7ecf1da53d82
benjobs <benjobs@qq.com>
2017-01-18 00:16:23 +0800

    fixbug

--- opencron-server/src/main/java/org/opencron/server/service/TerminalService.java
+++ opencron-server/src/main/java/org/opencron/server/service/TerminalService.java
@@ -442,7 +442,7 @@ public class TerminalService {
 
     public static class OpencronSftpMonitor extends TimerTask implements SftpProgressMonitor {
 
-        private long progressInterval = 5 * 1000; // 默认间隔时间为5秒
+        private long progressInterval = 2 * 1000; // 默认间隔时间为5秒
 
         private boolean isEnd = false; // 记录传输是否结束
 
@@ -461,60 +461,50 @@ public class TerminalService {
         @Override
         public void run() {
             if (!isEnd()) { // 判断传输是否已结束
-                System.out.println("Transfering is in progress.");
                 long transfered = getTransfered();
                 if (transfered != fileSize) { // 判断当前已传输数据大小是否等于文件总大小
-                    System.out.println("Current transfered: " + transfered + " bytes");
                     sendProgressMessage(transfered);
                 } else {
-                    System.out.println("File transfering is done.");
+                    logger.info("[opencron] Sftp file transfering is done.");
                     setEnd(true); // 如果当前已传输数据大小等于文件总大小，说明已完成，设置end
                 }
             } else {
-                System.out.println("Transfering done. Cancel timer.");
+                logger.info("[opencron] Sftp file transfering is done.cancel timer");
                 stop(); // 如果传输结束，停止timer记时器
                 return;
             }
         }
 
         public void stop() {
-            System.out.println("Try to stop progress monitor.");
+            logger.info("[opencron] Sftp stopping progress monitor.");
             if (timer != null) {
                 timer.cancel();
                 timer.purge();
                 timer = null;
                 isScheduled = false;
             }
-            System.out.println("Progress monitor stoped.");
+            logger.info("[opencron] Sftp progress monitor stoped.");
         }
 
         public void start() {
-            System.out.println("Try to start progress monitor.");
+            logger.info("[opencron] Sftp progress monitor Starting...");
             if (timer == null) {
                 timer = new Timer();
             }
             timer.schedule(this, 1000, progressInterval);
             isScheduled = true;
-            System.out.println("Progress monitor started.");
         }
 
-        /**
-         * 打印progress信息
-         * @param transfered
-         */
         private void sendProgressMessage(long transfered) {
             if (fileSize != 0) {
                 double d = ((double) transfered * 100) / (double) fileSize;
                 DecimalFormat df = new DecimalFormat("#.##");
-                System.out.println("Sending progress message: " + df.format(d) + "%");
+                logger.info("[opencron] Sftp Sending progress message: {} %",df.format(d));
             } else {
-                System.out.println("Sending progress message: " + transfered);
+                logger.info("[opencron] Sftp Sending progress message: ",transfered);
             }
         }
 
-        /**
-         * 实现了SftpProgressMonitor接口的count方法
-         */
         public boolean count(long count) {
             if (isEnd()) return false;
             if (!isScheduled) {
@@ -524,12 +514,8 @@ public class TerminalService {
             return true;
         }
 
-        /**
-         * 实现了SftpProgressMonitor接口的end方法
-         */
         public void end() {
             setEnd(true);
-            System.out.println("transfering end.");
         }
 
         private synchronized void add(long count) {
