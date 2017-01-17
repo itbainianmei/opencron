4f5b31c4739a294acadebc1dfeb67a4c12b316dd
benjobs <benjobs@qq.com>
2017-01-17 16:45:44 +0800

    fixbug

--- opencron-server/src/main/java/org/opencron/server/controller/TerminalController.java
+++ opencron-server/src/main/java/org/opencron/server/controller/TerminalController.java
@@ -22,7 +22,6 @@
 package org.opencron.server.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.mysql.jdbc.log.LogUtils;
 import org.opencron.common.utils.CommonUtils;
 import org.opencron.common.utils.WebUtils;
 import org.opencron.server.domain.Terminal;
@@ -35,13 +34,9 @@ import org.opencron.server.tag.PageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
